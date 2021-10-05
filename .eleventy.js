1fff4bea8f73565fb12fe971eaf9ef5330cd8ae1
Juga Paazmaya <paazmaya@yahoo.com>
2021-10-05 23:20:14 +0300

    Looks like I did a lot at some point, but when was it?

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
-module.exports = {
-  dir: {
-    includes: "partials",
-    layouts: "templates",
-    input: "src",
-    output: "dist"
-  }
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy("src/icons");
+
+  return {
+    dir: {
+      includes: "partials",
+      layouts: "templates",
+      input: "src",
+      output: "dist"
+    }
+  };
 };
