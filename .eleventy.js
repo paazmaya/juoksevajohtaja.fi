5e3ede7bcf86e445ddb52d50e98bc7786f88d75d
Juga Paazmaya <paazmaya@yahoo.com>
2024-12-16 19:32:04 +0200

    Cleaning up linting issues

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addPassthroughCopy('src/icons');
+	eleventyConfig.addPassthroughCopy("src/icons");
 
-  return {
-    dir: {
-      includes: 'partials',
-      layouts: 'templates',
-      input: 'src',
-      output: 'dist'
-    }
-  };
+	return {
+		dir: {
+			includes: "partials",
+			layouts: "templates",
+			input: "src",
+			output: "dist",
+		},
+	};
 };
